3696be8a251c1543d3c5df4390c0b636fc4cbf1c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-03-28 10:48:17 -0400

    Create ErrorfreeArithmetic.jl

---
+++ src/ErrorfreeArithmetic.jl
@@ -0,0 +1,15 @@
+module ErrorfreeArithmetic
+
+
+export eeftSum2inOrder, eftSum3inOrder, eftSum2, eftSum3, 
+       eftDiff2inOrder, eftDiff2, 
+       eftSquare, eftCube,
+       eftProd2, eftProd3,
+       eftInv, eftDiv2,
+       eftFMA, eftFMS,
+       eftSqrt, eftInvSqrt,
+       eftHorner
+
+using Polynomials
+
+end # module
