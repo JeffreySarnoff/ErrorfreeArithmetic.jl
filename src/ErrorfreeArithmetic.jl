75e43b050910fce40a8efa9a020761e118ee76ec
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-07 00:19:19 -0400

    Update ErrorfreeArithmetic.jl

--- src/ErrorfreeArithmetic.jl
+++ src/ErrorfreeArithmetic.jl
@@ -5,10 +5,7 @@ export eeftSum2inOrder, eftSum3inOrder, eftSum2, eftSum3,
        eftDiff2inOrder, eftDiff2, 
        eftSquare, eftCube,
        eftProd2, eftProd3,
-       eftInv, eftDiv2,
-       eftFMA, eftFMS,
-       eftSqrt, eftInvSqrt,
-       eftHorner,
+       eftInv, eftFMA, eftFMS,
        eftSum2Cplx, eftDiff2Cplx, eftProd2Cplx
 
 using Polynomials
