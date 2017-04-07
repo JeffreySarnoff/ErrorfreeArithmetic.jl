8813cb2fd8039d34de2b3a035db9c8ff9ee3d6d3
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-07 07:47:33 -0400

    Update ErrorfreeArithmetic.jl

--- src/ErrorfreeArithmetic.jl
+++ src/ErrorfreeArithmetic.jl
@@ -1,32 +1,31 @@
 module ErrorfreeArithmetic
 
 
-export eftSum2inOrder, eftSum3inOrder, eftSum2, eftSum3, 
-       eftDiff2inOrder, eftDiff2, 
-       eftSquare, eftCube,
-       eftProd2, eftProd3,
-       eftInv, eftDiv2, 
-       eftFMA, eftFMS,
-       eftSum2Cplx, eftDiff2Cplx, eftProd2Cplx
-
+export errorfree_sum2_inorder, errorfree_sum3_inorder,
+       errorfree_sum2, errorfree_sum3, 
+       errorfree_diff2_inorder, errorfree_diff2, 
+       errorfree_square, errorfree_cube,
+       errorfree_prod2, errorfree_prod3,
+       errorfree_inv, errorfree_div2, 
+       errorfree_fma, errorfree_fms
 
 #= single parameter error-free transformations =#
 
 # 'y' must be negated to get the right result
-function eftInv{T<:AbstractFloat}(a::T)
+function errorfree_inv{T<:AbstractFloat}(a::T)
      x = one(T) / a
      y = -(fma(x, a, -one(T)) / a)
      return x, y
 end
 
 
-function eftSquare{T<:AbstractFloat}(a::T)
+function errorfree_square{T<:AbstractFloat}(a::T)
     x = a * a
     y = fma(a, a, -x)
     return x, y
 end
 
-function eftCube{T<:AbstractFloat}(a::T)
+function errorfree_cube{T<:AbstractFloat}(a::T)
     p = a*a; e = fma(a, a, -p)
     x = p*a; p = fma(p, a, -x)
     y = e*a
@@ -35,53 +34,53 @@ end
 
 #= two parameter error-free transformations =#
 
-function eftSum2inOrder{T<:AbstractFloat}(a::T, b::T)
+function errorfree_sum2_inorder{T<:AbstractFloat}(a::T, b::T)
   x = a + b
   y = b - (x - a)
   return x, y
 end
 
-function eftSum2{T<:AbstractFloat}(a::T, b::T)
+function errorfree_sum2{T<:AbstractFloat}(a::T, b::T)
   x = a + b
   t = x - a
   y = (a - (x - t)) + (b - t)
   return x, y
 end
 
-eftSum2{T<:AbstractFloat, R<:Real}(a::T, b::R) = eftSum2(a, convert(T,b))
-eftSum2{T<:AbstractFloat, R<:Real}(a::R, b::T) = eftSum2(convert(T,a), b)
-eftSum2(a::Real, b::Real) = eftSum2(float(a), float(b))
+errorfree_sum2{T<:AbstractFloat, R<:Real}(a::T, b::R) = errorfree_sum2(a, convert(T,b))
+errorfree_sum2{T<:AbstractFloat, R<:Real}(a::R, b::T) = errorfree_sum2(convert(T,a), b)
+errorfree_sum2(a::Real, b::Real) = errorfree_sum2(float(a), float(b))
 
-function eftDiff2inOrder{T<:AbstractFloat}(a::T, b::T)
+function errorfree_diff2_inorder{T<:AbstractFloat}(a::T, b::T)
   x = a - b
   y = (a - x) - b
   return x, y
 end
 
-function eftDiff2{T<:AbstractFloat}(a::T, b::T)
+function errorfree_diff2{T<:AbstractFloat}(a::T, b::T)
   x = a - b
   t = x - a
   y = (a - (x - t)) - (b + t)
   return x, y
 end
 
-eftDiff2{T<:AbstractFloat, R<:Real}(a::T, b::R) = eftDiff2(a, convert(T,b))
-eftDiff2{T<:AbstractFloat, R<:Real}(a::R, b::T) = eftDiff2(convert(T,a), b)
-eftDiff2(a::Real, b::Real) = eftDiff2(float(a), float(b))
+errorfree_diff2{T<:AbstractFloat, R<:Real}(a::T, b::R) = errorfree_diff2(a, convert(T,b))
+errorfree_diff2{T<:AbstractFloat, R<:Real}(a::R, b::T) = errorfree_diff2(convert(T,a), b)
+errorfree_diff2(a::Real, b::Real) = errorfree_diff2(float(a), float(b))
 
 
-function eftProd2{T<:AbstractFloat}(a::T, b::T)
+function errorfree_prod2{T<:AbstractFloat}(a::T, b::T)
     x = a * b
     y = fma(a, b, -x)
     return x, y
 end
 
-eftProd2{T<:AbstractFloat, R<:Real}(a::T, b::R) = eftProd2(a, convert(T, b))
-eftProd2{T<:AbstractFloat, R<:Real}(a::R, b::T) = eftProd2(convert(T, a), b)
-eftProd2(a::Real, b::Real) = efProd2(float(a), float(b))
+errorfree_prod2{T<:AbstractFloat, R<:Real}(a::T, b::R) = errorfree_prod2(a, convert(T, b))
+errorfree_prod2{T<:AbstractFloat, R<:Real}(a::R, b::T) = errorfree_prod2(convert(T, a), b)
+errorfree_prod2(a::Real, b::Real) = errorfree_prod2(float(a), float(b))
 
 #=
- div is as good as possible, not quite eft
+ div is as good as possible, not quite errorfree_
  
 "Concerning the division, the elementary rounding error is
 generally not a floating point number, so it cannot be computed
@@ -99,7 +98,7 @@ we can expect in the working precision."
        end
 =#
 # 'y' must be negated to get the right result
-function eftDiv2{T<:AbstractFloat}(a::T,b::T)
+function errorfree_Div2{T<:AbstractFloat}(a::T,b::T)
      x = a / b
      y = -(fma(x, b,-a) / b)
      return x, y
@@ -108,46 +107,46 @@ end
 
 #= three parameter error-free transformations =#
 
-function eftSum3inOrder{T<:AbstractFloat}(a::T,b::T,c::T)
-    s, t = eftSum2inOrder(b, c)
-    x, u = eftSum2inOrder(a, s)
-    y, z = eftSum2inOrder(u, t)
-    x, y = eftSum2inOrder(x, y)
+function errorfree_sum3_inorder{T<:AbstractFloat}(a::T,b::T,c::T)
+    s, t = errorfree_sum2_inorder(b, c)
+    x, u = errorfree_sum2_inorder(a, s)
+    y, z = errorfree_sum2_inorder(u, t)
+    x, y = errorfree_sum2_inorder(x, y)
     return x, y, z
 end
 
-function eftSum3{T<:AbstractFloat}(a::T,b::T,c::T)
-    s, t = eftSum2(b, c)
-    x, u = eftSum2(a, s)
-    y, z = eftSum2(u, t)
-    x, y = eftSum2inOrder(x, y)
+function errorfree_sum3{T<:AbstractFloat}(a::T,b::T,c::T)
+    s, t = errorfree_sum2(b, c)
+    x, u = errorfree_sum2(a, s)
+    y, z = errorfree_sum2(u, t)
+    x, y = errorfree_sum2_inorder(x, y)
     return x, y, z
 end
 
-function eftProd3{T<:AbstractFloat}(a::T, b::T, c::T)
-    p, e = eftProd2(a, b)
-    x, p = eftProd2(p, c)
-    y, z = eftProd2(e, c)
+function errorfree_prod3{T<:AbstractFloat}(a::T, b::T, c::T)
+    p, e = errorfree_prod2(a, b)
+    x, p = errorfree_prod2(p, c)
+    y, z = errorfree_prod2(e, c)
     return x, y, z
 end
 
-function eftFMA{T<:AbstractFloat}(a::T, b::T, c::T)
+function errorfree_fma{T<:AbstractFloat}(a::T, b::T, c::T)
     x = fma(a, b, c)
-    u1, u2 = eftProd2(a, b)
-    a1, a2 = eftSum2(u2, c)
-    b1, b2 = eftSum2(u1, a1)
+    u1, u2 = errorfree_prod2(a, b)
+    a1, a2 = errorfree_sum2(u2, c)
+    b1, b2 = errorfree_sum2(u1, a1)
     g = (b1 - x) + b2
-    y, z = eftSum2inOrder(g, a2)
+    y, z = errorfree_sum2_inorder(g, a2)
     return x, y, z
 end
 
-function eftFMS{T<:AbstractFloat}(a::T, b::T, c::T)
+function errorfree_fms{T<:AbstractFloat}(a::T, b::T, c::T)
     x = fma(a, b, c)
-    u1, u2 = eftProd2(a, b)
-    a1, a2 = eftDiff2(u2, c)
-    b1, b2 = eftSum2(u1, a1)
+    u1, u2 = errorfree_prod2(a, b)
+    a1, a2 = errorfree_diff2(u2, c)
+    b1, b2 = errorfree_sum2(u1, a1)
     g = (b1 - x) + b2
-    y,z = eftSum2inOrder(g, a2)
+    y,z = errorfree_sum2_inorder(g, a2)
     return x, y, z
 end
 
@@ -161,27 +160,27 @@ end
   http://web.stanford.edu/group/SOL/software/qdotdd/IC2012.pdf
 =#
 
-function eftSum2Cplx{T<:AbstractFloat}(x::Complex{T}, y::Complex{T})
-    rhi, ihi = eftSum2(x.re, y.re)
-    rlo, ilo = eftSum2(x.im, y.im)
+function errorfree_sum2{T<:AbstractFloat}(x::Complex{T}, y::Complex{T})
+    rhi, ihi = errorfree_sum2(x.re, y.re)
+    rlo, ilo = errorfree_sum2(x.im, y.im)
     return Complex(rhi,rlo), Complex(ihi,ilo)
 end
 
-function eftDiff2Cplx{T<:AbstractFloat}(x::Complex{T}, y::Complex{T})
-    rhi, ihi = eftSum2(x.re, -y.re)
-    rlo, ilo = eftSum2(x.im, -y.im)
+function errorfree_diff2{T<:AbstractFloat}(x::Complex{T}, y::Complex{T})
+    rhi, ihi = errorfree_sum2(x.re, -y.re)
+    rlo, ilo = errorfree_sum2(x.im, -y.im)
     return Complex(rhi,rlo), Complex(ihi,ilo)
 end
 
 
-function eftProd2Cplx{T<:AbstractFloat}(x::Complex{T}, y::Complex{T})
-    z1, h1 = eftProd2(x.re, y.re)
-    z2, h2 = eftProd2(x.im, y.im)
-    z3, h3 = eftProd2(x.re, y.im)
-    z4, h4 = eftProd2(x.im, y.re)
+function errorfree_prod2{T<:AbstractFloat}(x::Complex{T}, y::Complex{T})
+    z1, h1 = errorfree_prod2(x.re, y.re)
+    z2, h2 = errorfree_prod2(x.im, y.im)
+    z3, h3 = errorfree_prod2(x.re, y.im)
+    z4, h4 = errorfree_prod2(x.im, y.re)
     
-    z5, h5 = eftSum2(z1, -z2)
-    z6, h6 = eftSum2(z3,  z4)
+    z5, h5 = errorfree_sum2(z1, -z2)
+    z6, h6 = errorfree_sum2(z3,  z4)
     
     return Complex(z5,z6), Complex(hi,h3), Complex(-h2,h4), Complex(h5,h6)
 end
