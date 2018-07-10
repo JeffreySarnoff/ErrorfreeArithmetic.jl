fa7ce149331232fac4e26869fb3e4d6d47e87bc6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-10 03:44:18 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,78 +8,78 @@ end
 
 BigFloat(x::T) where {T<:IEEEFloat} = Base.convert(BigFloat, x)
 
-function hilo(::Type{T} x::BigFloat) where {T<:IEEEFloat}
+function hilo(::Type{T} x::BigFloat) where {T}
     hi = T(x)
     lo = T(x - hi)
     return hi, lo
 end
 
-function himdlo(::Type{T} x::BigFloat) where {T<:IEEEFloat}
+function himdlo(::Type{T} x::BigFloat) where {T}
     hi = T(x)
     md = T(x - hi)
     lo = T(x - hi - md)
     return hi, md, lo
 end
 
-function calc_two_sum(a::T, b::T) where {T<:IEEEFloat}
+function calc_two_sum(a::T, b::T) where {T}
     aa, bb = BigFloat(a), BigFloat(b)
     ab = aa + bb
     return hilo(T, ab)
 end
 
-function calc_two_diff(a::T, b::T) where {T<:IEEEFloat}
+function calc_two_diff(a::T, b::T) where {T}
     aa, bb = BigFloat(a), BigFloat(b)
     ab = aa - bb
     return hilo(T, ab)
 end
 
-function calc_two_prod(a::T, b::T) where {T<:IEEEFloat}
+function calc_two_prod(a::T, b::T) where {T}
     aa, bb = BigFloat(a), BigFloat(b)
     ab = aa * bb
     return hilo(T, ab)
 end
 
-function calc_two_div(a::T, b::T) where {T<:IEEEFloat}
+function calc_two_div(a::T, b::T) where {T}
     aa, bb = BigFloat(a), BigFloat(b)
     ab = aa / bb
     return hilo(T, ab)
 end
 
-function calc_two_sqrt(a::T) where {T<:IEEEFloat}
+function calc_two_sqrt(a::T) where {T}
     aa = BigFloat(a)
     ab = sqrt(aa)
     return hilo(T, ab)
 end
 
-function isclosest(lo::T, low::T) where {T<:IEEEFloat}
+function isclosest(lo::T, low::T) where {T}
     lo === low || ((abs(lo - low) <= abs(lo - nextfloat(low))) && (abs(lo - low) <= abs(lo - prevfloat(low))))
 end
 
-function test_two_sum(a::T, b::T) where {T<:IEEEFloat}
+function test_two_sum(a::T, b::T) where {T}
     hi, lo = two_sum(a, b)
     high, low = calc_two_sum(a, b)
     hi === high && lo === low
 end
 
-function test_two_diff(a::T, b::T) where {T<:IEEEFloat}
+function test_two_diff(a::T, b::T) where {T}
     hi, lo = two_diff(a, b)
     high, low = calc_two_diff(a, b)
     hi === high && lo === low
 end
 
-function test_two_prod(a::T, b::T) where {T<:IEEEFloat}
+function test_two_prod(a::T, b::T) where {T}
     hi, lo = two_prod(a, b)
     high, low = calc_two_prod(a, b)
     hi === high && lo === low
 end
 
-function test_two_sqrt(a::T) where {T<:IEEEFloat}
+function test_two_sqrt(a::T) where {T}
     hi, lo = two_sqrt(a)
     high, low = calc_two_sqrt(a)     
     hi === high && isclosest(lo, low)
 end
 
-function test_two_div(a::T, b::T) where {T<:IEEEFloat}
+function test_two_div(a::T, b::T) where {T}
     hi, lo = two_div(a, b)
     high, low = calc_two_div(a, b)     
     hi === high && isclosest(lo, low)
