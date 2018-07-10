7d391d5d612b8db79683f575c1f89d816dcb6929
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-10 02:30:32 -0400

    Update ErrorfreeArithmetic.jl

--- src/ErrorfreeArithmetic.jl
+++ src/ErrorfreeArithmetic.jl
@@ -4,7 +4,7 @@ export two_sum, two_diff, two_prod, two_inv, two_div,
        two_sum_sorted, two_diff_sorted,
        three_sum, three_diff, three_prod,
        three_sum_sorted, three_diff_sorted,
-       two_sqrt, two_cbrt, three_cbrt
+       two_sqrt, two_cbrt, three_cbrt,
        three_fma
 
 include("errorfree.jl")
