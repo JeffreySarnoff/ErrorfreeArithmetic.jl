937b4ca6744a5893ccc6678cfeab7d6608a6a6bb
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-20 14:51:05 -0400

    Update errorfree.jl

--- src/errorfree.jl
+++ src/errorfree.jl
@@ -1,17 +1,4 @@
 """
-    two_sum_sorted(a, b)
-
-*unchecked* requirement `|a| ≥ |b|`
-
-Computes `s = fl(a+b)` and `e = err(a+b)`.
-"""
-@inline function two_sum_sorted(a::T, b::T) where {T<:AbstractFloat}
-    s = a + b
-    e = b - (s - a)
-    return s, e
-end
-
-"""
     two_sum(a, b)
 
 Computes `s = fl(a+b)` and `e = err(a+b)`.
@@ -21,20 +8,6 @@ Computes `s = fl(a+b)` and `e = err(a+b)`.
     v = s - a
     e = (a - (s - v)) + (b - v)
     return s, e
-end
-
-
-"""
-    two_diff_sorted(a, b)
-    
-*unchecked* requirement `|a| ≥ |b|`
-
-Computes `s = fl(a-b)` and `e = err(a-b)`.
-"""
-@inline function two_diff_sorted(a::T, b::T) where {T<:AbstractFloat}
-    s = a - b
-    e = (a - s) - b
-    s, e
 end
 
 """
@@ -49,7 +22,6 @@ Computes `s = fl(a-b)` and `e = err(a-b)`.
 
     s, e
 end
-
 
 """
     two_square(a)
@@ -86,21 +58,6 @@ Computes `s = fl(a*b)` and `e = err(a*b)`.
 end
 
 """
-    three_sum_sorted(a, b, c)
-    
-*unchecked* requirement `|a| ≥ |b| ≥ |c|`
-
-Computes `s = fl(a+b+c)` and `e1 = err(a+b+c), e2 = err(e1)`.
-"""
-function three_sum_sorted(a::T,b::T,c::T) where {T<:AbstractFloat}
-    s, t = two_sum_sorted(b, c)
-    x, u = two_sum_sorted(a, s)
-    y, z = two_sum_sorted(u, t)
-    x, y = two_sum_sorted(x, y)
-    return x, y, z
-end
-
-"""
     three_sum(a, b, c)
     
 Computes `s = fl(a+b+c)` and `e1 = err(a+b+c), e2 = err(e1)`.
@@ -109,22 +66,7 @@ function three_sum(a::T,b::T,c::T) where {T<:AbstractFloat}
     s, t = two_sum(b, c)
     x, u = two_sum(a, s)
     y, z = two_sum(u, t)
-    x, y = two_sum_sorted(x, y)
-    return x, y, z
-end
-
-"""
-    three_diff_sorted(a, b, c)
-    
-*unchecked* requirement `|a| ≥ |b| ≥ |c|`
-
-Computes `s = fl(a-b-c)` and `e1 = err(a-b-c), e2 = err(e1)`.
-"""
-function three_diff_sorted(a::T,b::T,c::T) where {T<:AbstractFloat}
-    s, t = two_diff_sorted(-b, c)
-    x, u = two_sum_sorted(a, s)
-    y, z = two_sum_sorted(u, t)
-    x, y = two_sum_sorted(x, y)
+    x, y = two_hilo_sum(x, y)
     return x, y, z
 end
 
@@ -138,7 +80,7 @@ function three_diff(a::T,b::T,c::T) where {T<:AbstractFloat}
     s, t = two_diff(-b, c)
     x, u = two_sum(a, s)
     y, z = two_sum(u, t)
-    x, y = two_sum_sorted(x, y)
+    x, y = two_hilo_sum(x, y)
     return x, y, z
 end
 
@@ -152,7 +94,7 @@ function three_prod(a::T, b::T, c::T) where {T<:AbstractFloat}
     y, z = two_prod(a, b)
     x, y = two_prod(y, c)
     z, t = two_(z, c)
-    y, z = two_sum_sorted(y, z)
+    y, z = two_hilo_sum(y, z)
     z += t
     return x, y, z
 end
@@ -167,10 +109,16 @@ function three_cube(a::T) where {T<:AbstractFloat}
     y, z = two_prod(a, a)
     x, y = two_prod(y, a)
     z, t = two_prod(z, a)
-    y, z = two_sum_sorted(y, z)
+    y, z = two_hilo_sum(y, z)
     z += t
     return x, y, z
 end
+
+#=
+   three_fma algorithm from
+   Sylvie Boldo and Jean-Michel Muller
+   Some Functions Computable with a Fused-mac
+=#
 
 """
    three_fma(a, b, c)
@@ -183,13 +131,123 @@ function three_fma(a::T, b::T, c::T) where {T<:IEEEFloat}
      t, z = two_sum(c, z)
      t, u = two_sum(y, t)
      y = ((t - x) + u)
-     y, z = two_sum_sorted(y, z)
+     y, z = two_hilo_sum(y, z)
      return x, y, z
 end
 
+# with arguments sorted by magnitude
 
-#=
-   three_fma algorithm from
-   Sylvie Boldo and Jean-Michel Muller
-   Some Functions Computable with a Fused-mac
-=#
+"""
+    two_hilo_sum(a, b)
+
+*unchecked* requirement `|a| ≥ |b|`
+
+Computes `s = fl(a+b)` and `e = err(a+b)`.
+"""
+@inline function two_hilo_sum(a::T, b::T) where {T<:AbstractFloat}
+    s = a + b
+    e = b - (s - a)
+    return s, e
+end
+
+"""
+    two_lohi_sum(a, b)
+
+*unchecked* requirement `|b| ≥ |a|`
+
+Computes `s = fl(a+b)` and `e = err(a+b)`.
+"""
+@inline function two_lohi_sum(a::T, b::T) where {T<:AbstractFloat}
+    s = b + a
+    e = a - (s - b)
+    return s, e
+end
+
+"""
+    two_hilo_diff(a, b)
+    
+*unchecked* requirement `|a| ≥ |b|`
+
+Computes `s = fl(a-b)` and `e = err(a-b)`.
+"""
+@inline function two_hilo_diff(a::T, b::T) where {T<:AbstractFloat}
+    s = a - b
+    e = (a - s) - b
+    s, e
+end
+
+"""
+    two_lohi_diff(a, b)
+    
+*unchecked* requirement `|b| ≥ |a|`
+
+Computes `s = fl(a-b)` and `e = err(a-b)`.
+"""
+@inline function two_lohi_diff(a::T, b::T) where {T<:AbstractFloat}
+    s = b - a
+    e = (b - s) - a
+    s, e
+end
+
+
+
+"""
+    three_hilo_sum(a, b, c)
+    
+*unchecked* requirement `|a| ≥ |b| ≥ |c|`
+
+Computes `s = fl(a+b+c)` and `e1 = err(a+b+c), e2 = err(e1)`.
+"""
+function three_hilo_sum(a::T,b::T,c::T) where {T<:AbstractFloat}
+    s, t = two_hilo_sum(b, c)
+    x, u = two_hilo_sum(a, s)
+    y, z = two_hilo_sum(u, t)
+    x, y = two_hilo_sum(x, y)
+    return x, y, z
+end
+
+"""
+    three_lohi_sum(a, b, c)
+    
+*unchecked* requirement `|c| ≥ |b| ≥ |a|`
+
+Computes `s = fl(a+b+c)` and `e1 = err(a+b+c), e2 = err(e1)`.
+"""
+function three_hilo_sum(a::T,b::T,c::T) where {T<:AbstractFloat}
+    s, t = two_hilo_sum(b, a)
+    x, u = two_hilo_sum(c, s)
+    y, z = two_hilo_sum(u, t)
+    x, y = two_hilo_sum(x, y)
+    return x, y, z
+end
+
+"""
+    three_hilo_diff(a, b, c)
+    
+*unchecked* requirement `|a| ≥ |b| ≥ |c|`
+
+Computes `s = fl(a-b-c)` and `e1 = err(a-b-c), e2 = err(e1)`.
+"""
+function three_hilo_diff(a::T,b::T,c::T) where {T<:AbstractFloat}
+    s, t = two_hilo_diff(-b, c)
+    x, u = two_hilo_sum(a, s)
+    y, z = two_hilo_sum(u, t)
+    x, y = two_hilo_sum(x, y)
+    return x, y, z
+end
+
+"""
+    three_lohi_diff(a, b, c)
+    
+*unchecked* requirement `|c| ≥ |b| ≥ |a|`
+
+Computes `s = fl(a-b-c)` and `e1 = err(a-b-c), e2 = err(e1)`.
+"""
+function three_hilo_diff(a::T,b::T,c::T) where {T<:AbstractFloat}
+    s, t = two_hilo_diff(-b, a)
+    x, u = two_hilo_sum(c, s)
+    y, z = two_hilo_sum(u, t)
+    x, y = two_hilo_sum(x, y)
+    return x, y, z
+end
+
