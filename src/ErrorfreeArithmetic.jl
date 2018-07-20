ad98cc8ff1d4072eb33c77eada3b7dec78254b1a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-20 14:38:53 -0400

    Update ErrorfreeArithmetic.jl

--- src/ErrorfreeArithmetic.jl
+++ src/ErrorfreeArithmetic.jl
@@ -1,11 +1,15 @@
 module ErrorfreeArithmetic
 
-export two_sum, two_diff, two_prod, two_inv, two_div,
-       two_sum_sorted, two_diff_sorted,
+# these exports implement error-free transformations
+export two_sum, two_diff, two_prod 
        three_sum, three_diff, three_prod,
+       two_sum_sorted, two_diff_sorted,
        three_sum_sorted, three_diff_sorted,
-       two_sqrt, two_cbrt, three_cbrt,
        three_fma
+
+# these exports implement least-error transformations
+# they are as close to error-free as possible
+export two_inv, two_sqrt, two_div
 
 import Base.IEEEFloat
 
