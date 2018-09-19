2712b1b33eb44d6bc3cfdc2ba0b477babd3713c1
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-09-19 03:01:49 -0400

    Update errorfree.jl

--- src/errorfree.jl
+++ src/errorfree.jl
@@ -1,13 +1,61 @@
 """
     two_sum(a, b)
 
-Computes `s = fl(a+b)` and `e = err(a+b)`.
+Computes `hi = fl(a+b)` and `lo = err(a+b)`.
 """
 @inline function two_sum(a::T, b::T) where {T<:AbstractFloat}
-    s = a + b
-    v = s - a
-    e = (a - (s - v)) + (b - v)
-    return s, e
+    hi = a + b
+    v  = hi - a
+    lo = (a - (hi - v)) + (b - v)
+    return hi, lo
+end
+
+"""
+    three_sum(a, b, c)
+    
+Computes `hi = fl(a+b+c)` and `md = err(a+b+c), lo = err(md)`.
+"""
+function three_sum(a::T,b::T,c::T) where {T<:AbstractFloat}
+    s, t   = two_sum(b, c)
+    hi, u  = two_sum(a, s)
+    md, lo = two_sum(u, t)
+    hi, md = two_hilo_sum(hi, md)
+    return hi, md, lo
+end
+
+"""
+    four_sum(a, b, c, d)
+    
+Computes `hi = fl(a+b+c+d)` and `hm = err(a+b+c+d), ml = err(hm), lo = err(ml)`.
+"""
+function four_sum(a::T,b::T,c::T,d::T) where {T<: AbstractFloat}
+    t0, t1 = two_sum(a ,  b)
+    t0, t2 = two_sum(t0,  c)
+    hi, t3 = two_sum(t0,  d)
+    t0, t1 = two_sum(t1, t2)
+    hm, t2 = two_sum(t0, t3)
+    ml, lo = two_sum(t1, t2)
+    return hi, hm, ml, lo
+end
+
+"""
+    five_sum(a, b, c, d, e)
+    
+Computes `s = fl(a+b+c+d+e)` and 
+    `e1 = err(a+b+c+d), e2 = err(e1), e3 = err(e2), e4 = err(e3)`.
+"""
+function five_sum(v::T, w::T, x::T, y::T, z::T) where {T<:AbstractFloat}
+    t0, t4 = two_sum(y, z)
+    t0, t3 = two_sum(x, t0)
+    t0, t2 = two_sum(w, t0)
+    a, t1  = two_sum(v, t0)
+    t0, t3 = two_sum(t3, t4)
+    t0, t2 = two_sum(t2, t0)
+    b, t1  = two_sum(t1, t0)
+    t0, t2 = two_sum(t2, t3)
+    c, t1  = two_sum(t1, t0)
+    d, e   = two_sum(t1, t2)
+    return a, b, c, d, e
 end
 
 """
@@ -57,53 +105,7 @@ Computes `s = fl(a*b)` and `e = err(a*b)`.
     p, e
 end
 
-"""
-    three_sum(a, b, c)
-    
-Computes `s = fl(a+b+c)` and `e1 = err(a+b+c), e2 = err(e1)`.
-"""
-function three_sum(a::T,b::T,c::T) where {T<:AbstractFloat}
-    s, t = two_sum(b, c)
-    x, u = two_sum(a, s)
-    y, z = two_sum(u, t)
-    x, y = two_hilo_sum(x, y)
-    return x, y, z
-end
 
-"""
-    four_sum(a, b, c, d)
-    
-Computes `s = fl(a+b+c+d)` and `e1 = err(a+b+c+d), e2 = err(e1), e3 = err(e2)`.
-"""
-function four_sum(a::T,b::T,c::T,d::T) where {T<: AbstractFloat}
-    t0, t1 = two_sum(a ,  b)
-    t0, t2 = two_sum(t0,  c)
-    a,  t3 = two_sum(t0,  d)
-    t0, t1 = two_sum(t1, t2)
-    b,  t2 = two_sum(t0, t3)
-    c,  d  = two_sum(t1, t2)
-    return a, b, c, d
-end
-
-"""
-    five_sum(a, b, c, d, e)
-    
-Computes `s = fl(a+b+c+d+e)` and 
-    `e1 = err(a+b+c+d), e2 = err(e1), e3 = err(e2), e4 = err(e3)`.
-"""
-function five_sum(v::T, w::T, x::T, y::T, z::T) where {T<:AbstractFloat}
-    t0, t4 = two_sum(y, z)
-    t0, t3 = two_sum(x, t0)
-    t0, t2 = two_sum(w, t0)
-    a, t1  = two_sum(v, t0)
-    t0, t3 = two_sum(t3, t4)
-    t0, t2 = two_sum(t2, t0)
-    b, t1  = two_sum(t1, t0)
-    t0, t2 = two_sum(t2, t3)
-    c, t1  = two_sum(t1, t0)
-    d, e   = two_sum(t1, t2)
-    return a, b, c, d, e
-end
 
 """
     three_diff(a, b, c)
