1f8bb4b5975ba3a7bf6177edf5c84b966fe3b933
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-05-23 01:06:57 -0400

    Update errorfree.jl

--- src/errorfree.jl
+++ src/errorfree.jl
@@ -32,7 +32,7 @@ function two_sum(a::T,b::T,c::T) where {T}
     hi1, lo1 = two_sum(b, c)
     hi2, lo2 = two_sum(a, hi1)
     lo12     = lo1 + lo2
-    hi, lo   = two_hilo_sum(hi2, lo2+lo1)
+    hi, lo   = two_hilo_sum(hi2, lo12)
     return hi, lo
 end
 
