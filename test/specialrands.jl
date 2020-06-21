79387760b679429641eec810ea78cda3afdd556a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-06-21 02:45:10 -0400

    Update specialrands.jl

--- test/specialrands.jl
+++ test/specialrands.jl
@@ -1,6 +1,7 @@
 # generate random 2,3,4-tuples designed to exercise error-free transformations
     
-fullseparation(::Type{T}) where {T} =  Base.significand_bits(T) + 2
+fullseparation(::Type{T}) where {T} =  Base.significand_bits(T) + 3
+bitoverlap(::Type{T}, nbits) where {T} = fullseparation(T) - nbits
 
 function tworands(::Type{T}, separation::Int=fullseparation(T)) where {T}
     hishift = fld(separation, rand(1:5))
@@ -10,3 +11,6 @@ function tworands(::Type{T}, separation::Int=fullseparation(T)) where {T}
     return hi, lo
 end
 
+function tworandsoverlap(::Type{T}, overlap) where {T}
+    return tworands(T, bitoverlap(T, overlap))
+end
