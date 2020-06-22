9755e07aaebdff945b5c4c34b7a90eb927073405
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-06-22 03:00:20 -0400

    Create errorfree_tuple.jl

---
+++ src/errorfree_tuple.jl
@@ -0,0 +1,19 @@
+two_sum(x::NTuple{N,T})   where {T,N} = two_sum(x...)
+three_sum(x::NTuple{N,T}) where {T,N} = three_sum(x...)
+four_sum(x::NTuple{N,T})  where {T,N} = four_sum(x...)
+
+two_diff(x::NTuple{N,T})   where {T,N} = two_diff(x...)
+three_diff(x::NTuple{N,T}) where {T,N} = three_diff(x...)
+four_diff(x::NTuple{N,T})  where {T,N} = four_diff(x...)
+
+two_prod(x::NTuple{N,T})   where {T,N} = two_prod(x...)
+three_prod(x::NTuple{N,T}) where {T,N} = three_prod(x...)
+four_prod(x::NTuple{N,T})  where {T,N} = four_prod(x...)
+
+two_inv(x::NTuple{N,T})   where {T,N} = two_inv(x...)
+three_inv(x::NTuple{N,T}) where {T,N} = three_inv(x...)
+four_inv(x::NTuple{N,T})  where {T,N} = four_inv(x...)
+
+two_divide(x::NTuple{N,T})   where {T,N} = two_divide(x...)
+three_divide(x::NTuple{N,T}) where {T,N} = three_divide(x...)
+four_divide(x::NTuple{N,T})  where {T,N} = four_divide(x...)
