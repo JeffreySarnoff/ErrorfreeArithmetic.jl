508093094da69521d2387d57c5103a29d0f8f3c2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-06-22 02:32:40 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using ErrorfreeArithmetic
 using Test
 
+# 512. (sigbits + extrabits) * (4 + 1) [4+1 from renormalize(a,b,c,d,e)]
+const BigFloatOldPrec = precision(BigFloat)
+const BigFloatPrec = nextpow(2, ((Base.significand_bits(Float64) + 1 + 36) * 5)) 
+setprecision(BigFloat, BigFloatPrec)
+
+# select one of these two approaches
+# bigfloat(x) = BigFloat(string(x))
+bigfloat(x) = BigFloat(x)
+
+# ----------
+
 include("specialrands.jl")
 include("permutations.jl")
 include("errorfree.jl")
@@ -8,3 +19,7 @@ include("errorfree.jl")
 include("resolvefloats.jl")
 
 include("test_n_func.jl")
+
+# ----------
+
+setprecision(BigFloat, BigFloatOldPrec)
