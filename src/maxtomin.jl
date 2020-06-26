f0c840a669ab5bddba6fc750c43d9910a5c50c4b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-06-26 15:54:20 -0400

    Update maxtomin.jl

--- src/maxtomin.jl
+++ src/maxtomin.jl
@@ -22,3 +22,28 @@ end
 magnitude_maxtomin(x::NTuple{2, T}) where {T} = magnitude_maxtomin(x[1], x[2])
 magnitude_maxtomin(x::NTuple{3, T}) where {T} = magnitude_maxtomin(x[1], x[2], x[3])
 magnitude_maxtomin(x::NTuple{4, T}) where {T} = magnitude_maxtomin(x[1], x[2], x[3], x[4])
+
+inline magnitude_minmax(x, y) = abs(x) < abs(y) ? (x, y) : (y, x)
+
+magnitude_mintomax(a, b) = magnitude_minnax(a, b)
+
+function magnitude_mintomax(a, b, c)
+    b, c = magnitude_minmax(b, c)
+    a, c = magnitude_minmax(a, c)
+    a, b = magnitude_minmax(a, b)
+    return a, b, c
+end
+
+function magnitude_mintomax(a, b, c, d)
+    c, d = magnitude_minmax(c, d)
+    b, d = magnitude_minmax(b, d)
+    a, d = magnitude_minmax(a, d)    
+    b, c = magnitude_minmax(b, c)
+    a, c = magnitude_minmax(a, c)
+    a, b = magnitude_minmax(a, b)
+    return a, b, c, d
+end
+
+magnitude_mintomax(x::NTuple{2, T}) where {T} = magnitude_mintomax(x[1], x[2])
+magnitude_mintomax(x::NTuple{3, T}) where {T} = magnitude_mintomax(x[1], x[2], x[3])
+magnitude_mintomax(x::NTuple{4, T}) where {T} = magnitude_mintomax(x[1], x[2], x[3], x[4])
