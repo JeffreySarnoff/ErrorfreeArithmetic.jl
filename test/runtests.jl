83978d6415a751d71aa9fbb0562e75c747bd11c6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-07-11 15:51:14 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,15 +2,6 @@ using ErrorfreeArithmetic, Random, Test
 
 Random.seed!(0xface)  # ensure tests that use rands are repeatable
 
-# 512. (sigbits + extrabits) * (4 + 1) [4+1 from renormalize(a,b,c,d,e)]
-const BigFloatOldPrec = precision(BigFloat)
-const BigFloatPrec = nextpow(2, ((Base.significand_bits(Float64) + 1 + 36) * 5)) 
-setprecision(BigFloat, BigFloatPrec)
-
-# select one of these two approaches
-# bigfloat(x) = BigFloat(string(x))
-bigfloat(x) = BigFloat(x)
-
 # ----------
 trials = 100
 # ----------
