ce9cc963de84dec7fbecb16d84d77123195fec10
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-07-11 14:09:40 -0400

    Update ErrorfreeArithmetic.jl

--- src/ErrorfreeArithmetic.jl
+++ src/ErrorfreeArithmetic.jl
@@ -1,20 +1,29 @@
 module ErrorfreeArithmetic
 
 export SysFloat,
-       # error-free transformations
-       two_sum, two_diff, two_prod, two_square,
-       three_sum, three_diff, three_prod,
-       four_sum, four_diff,
-       ad_minus_bc, two_fma, three_fma, two_muladd,
-       # error-free transformations with magnitude sorted arguments
-       two_hilo_sum, two_lohi_sum, two_hilo_diff, two_lohi_diff,
-       three_hilo_sum, three_lohi_sum, three_hilo_diff, three_lohi_diff,
-       four_hilo_sum, four_lohi_sum, four_hilo_diff, four_lohi_diff,
-       # least-error transformations, as close to error-free as possible
-       two_div,
-       two_inv, two_sqrt,
+       # error-free transformations (single argument)
+       two_square
+       # error-free transformations (unsorted arguments)
+       two_sum, three_sum, four_sum,
+       two_diff, three_diff, four_diff,
+       two_prod, three_prod, four_prod,
+       two_fma, three_fma.
+       # error-free transformations (magnitude sorted arguments)
+       two_hilo_sum, three_hilo_sum, four_hilo_sum,
+       two_hilo_diff, three_hilo_diff, four_hilo_diff,
+       two_lohi_sum, three_lohi_sum, four_lohi_sum,
+       two_lohi_diff, three_lohi_diff, four_lohi_diff,
        # error-free remainders
-       div_rem, sqrt_rem
+       div_rem, sqrt_rem,
+       # least-error transformations, as close to error-free as possible
+       # least-error transformations (single argument)
+       two_inv, two_sqrt,
+       # least-error transformations (two arguments)
+       two_div,
+       # least-error transformations (three arguments)
+       a_minus_bc. ab_minus_c,
+       # least-error transformations (four arguments)
+       ad_minus_bc
 
 
 using Base: IEEEFloat
