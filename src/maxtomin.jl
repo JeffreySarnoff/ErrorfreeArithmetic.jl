a91372ea62acf7ba5cc30f1e1986f4ebed7fc1b3
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-07-11 14:13:31 -0400

    Update maxtomin.jl

--- src/maxtomin.jl
+++ src/maxtomin.jl
@@ -1,11 +1,17 @@
-@inline magnitude_maxmin(x, y) = abs(y) < abs(x) ? (x, y) : (y, x)
-
-magnitude_maxtomin(a, b) = magnitude_maxmin(a, b)
+@inline magnitude_maxmin(x, y) = signbit(abs(y) - abs(x)) ? (x, y) : (y, x)
+@inline magnitude_minmax(x, y) = signbit(abs(x) - abs(y)) ? (x, y) : (y, x)
 
 function magnitude_maxtomin(a, b, c)
     b, c = magnitude_maxmin(b, c)
     a, c = magnitude_maxmin(a, c)
     a, b = magnitude_maxmin(a, b)
+    return a, b, c
+end
+
+function magnitude_mintomax(a, b, c)
+    b, c = magnitude_minmax(b, c)
+    a, c = magnitude_minmax(a, c)
+    a, b = magnitude_minmax(a, b)
     return a, b, c
 end
 
@@ -19,21 +25,6 @@ function magnitude_maxtomin(a, b, c, d)
     return a, b, c, d
 end
 
-magnitude_maxtomin(x::NTuple{2, T}) where {T} = magnitude_maxtomin(x[1], x[2])
-magnitude_maxtomin(x::NTuple{3, T}) where {T} = magnitude_maxtomin(x[1], x[2], x[3])
-magnitude_maxtomin(x::NTuple{4, T}) where {T} = magnitude_maxtomin(x[1], x[2], x[3], x[4])
-
-@inline magnitude_minmax(x, y) = abs(x) < abs(y) ? (x, y) : (y, x)
-
-magnitude_mintomax(a, b) = magnitude_minnax(a, b)
-
-function magnitude_mintomax(a, b, c)
-    b, c = magnitude_minmax(b, c)
-    a, c = magnitude_minmax(a, c)
-    a, b = magnitude_minmax(a, b)
-    return a, b, c
-end
-
 function magnitude_mintomax(a, b, c, d)
     c, d = magnitude_minmax(c, d)
     b, d = magnitude_minmax(b, d)
@@ -43,6 +34,11 @@ function magnitude_mintomax(a, b, c, d)
     a, b = magnitude_minmax(a, b)
     return a, b, c, d
 end
+
+
+magnitude_maxtomin(x::NTuple{2, T}) where {T} = magnitude_maxtomin(x[1], x[2])
+magnitude_maxtomin(x::NTuple{3, T}) where {T} = magnitude_maxtomin(x[1], x[2], x[3])
+magnitude_maxtomin(x::NTuple{4, T}) where {T} = magnitude_maxtomin(x[1], x[2], x[3], x[4])
 
 magnitude_mintomax(x::NTuple{2, T}) where {T} = magnitude_mintomax(x[1], x[2])
 magnitude_mintomax(x::NTuple{3, T}) where {T} = magnitude_mintomax(x[1], x[2], x[3])
