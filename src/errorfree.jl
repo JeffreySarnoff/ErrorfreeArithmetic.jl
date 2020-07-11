cf8899d0f154e151a2c54dfa480e4194468933fa
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-07-11 16:20:23 -0400

    Update errorfree.jl

--- src/errorfree.jl
+++ src/errorfree.jl
@@ -26,6 +26,19 @@ Computes `hi = fl(a+b)` and `lo = err(a+b)`.
 end
 
 """
+   two_sum(a, b, c)
+    
+Computes `hi = fl(a+b+c)` and `lo = err(a+b+c)`.
+"""
+function two_sum(a::T, b::T, c::T) where {T}
+    t0, t1 = two_sum(b, c) 
+    hi, t0 = two_sum(a, t0)
+    t0 += t1
+    hi, lo = two_hilo_sum(hi, t0)
+    return hi, lo
+end
+
+"""
    three_sum(a, b, c)
     
 Computes `hi = fl(a+b+c)` and `md = err(a+b+c), lo = err(md)`.
