51be435759c8f6033b86c73d7780309ff698cd0c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-07-11 21:02:07 -0400

    Update test_n_func.jl

--- test/test_n_func.jl
+++ test/test_n_func.jl
@@ -1,15 +1,38 @@
 a = sqrt(2.0)
 b = sqrt(987654.0)
-c = cbrt(456.125)
+c = cbrt(2.0)
+d = cbrt(987654.0)
 
-@test test_two_sum(a, b)
-@test test_two_diff(a, b)
-@test test_two_square(b)
-@test test_two_prod(a, b)
-@test test_two_inv(b)
-@test test_two_div(a, b)
+x, y = two_sum(a, b)
+@test x == a + b && x + y == x
+x, y = two_sum(a, b, c)
+@test x == a + b + c && x + y == x
+x, y = two_sum(a, b, c, d)
+@test x == a + b + c + d && x + y == x
+
+x, y = two_diff(a, b)
+@test x == a - b && x + y == x
+x, y = two_diff(a, b, c)
+@test x == a - b - c&& x + y == x
+x, y = two_diff(a, b, c, d)
+@test x == a - b - c - d && x + y == x
+
+x, y = two_square(a)
+@test x == a * a && x + y == x
+
+x, y = two_prod(a, b)
+@test x == a * b && x + y == x
+
+x, y = two_inv(a)
+@test prevfloat(inv(a)) <= x <= nextfloat(inv(a)) && x + y == x
+
+x, y = two_div(a, b)
+@test prevfloat(a/b) <= x <= nextfloat(a/b) && x + y == x
+
+#=
 @test test_two_sqrt(b)
 
 @test test_three_sum(a, b, c)
 @test test_three_diff(a, b, c)
 @test test_three_prod(a, b, c)
+=#
