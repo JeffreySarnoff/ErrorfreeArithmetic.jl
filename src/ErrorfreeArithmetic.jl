0ce856f3ebe27e849d2116c9b154d3220f4c3146
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-10-04 09:12:09 -0400

    Update ErrorfreeArithmetic.jl

--- src/ErrorfreeArithmetic.jl
+++ src/ErrorfreeArithmetic.jl
@@ -10,7 +10,7 @@ export # error-free transformations
        three_hilo_sum, three_lohi_sum, three_hilo_diff, three_lohi_diff,
        four_hilo_sum, four_lohi_sum, four_hilo_diff, four_lohi_diff,
        # least-error transformations, as close to error-free as possible
-       two_inv, two_sqrt, two_div
+       two_inv, two_sqrt, two_div,
        # error-free remainders
        div_rem, sqrt_rem
 
