6a83590ba8ceb9caf19cb3b77c2d1831d1887771
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-10-04 02:23:50 -0400

    Update testing.jl

--- test/testing.jl
+++ test/testing.jl
@@ -4,15 +4,18 @@ const EXPMAXS = (2, 4, 16, 32, 64, 96)
 trials1 = reshape(zeros(Float64, NTRIALS * length(EXPMAXS)), NTRIALS, length(EXPMAXS))
 trials2 = reshape(zeros(Float64, NTRIALS * length(EXPMAXS)), NTRIALS, length(EXPMAXS))
 trials3 = reshape(zeros(Float64, NTRIALS * length(EXPMAXS)), NTRIALS, length(EXPMAXS))
-
+trials4 = reshape(zeros(Float64, NTRIALS * length(EXPMAXS)), NTRIALS, length(EXPMAXS))
+trials5 = reshape(zeros(Float64, NTRIALS * length(EXPMAXS)), NTRIALS, length(EXPMAXS))
 
 for i in 1:NTRIALS
     for j in 1:length(EXPMAXS)
         expmax = EXPMAXS[j]
-        rands = randfloat(3; expmax)
+        rands = randfloat(5; expmax)
         trials1[i,j] = rands[1]
         trials2[i,j] = rands[2]
         trials3[i,j] = rands[3]
+        trials4[i,j] = rands[4]
+        trials5[i,j] = rands[5]
     end
 end
 
