91787d838ff03c10b97ec1df15cf49d6438174fe
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-10-04 17:41:13 -0400

    Update diff.jl

--- src/diff.jl
+++ src/diff.jl
@@ -100,3 +100,117 @@ function four_diff(a::T,b::T,c::T,d::T) where {T}
     hi, mdhi, mdlo, lo
 end
 
+
+# with arguments sorted by magnitude
+
+"""
+    two_hilo_sum(a, b)
+
+*unchecked* requirement `|a| ≥ |b|`
+
+Computes `hi = fl(a+b)` and `lo = err(a+b)`.
+"""
+@inline function two_hilo_sum(a::T, b::T) where {T}
+    hi = a + b
+    lo = b - (hi - a)
+    return hi, lo
+end
+
+"""
+    two_lohi_sum(a, b)
+
+*unchecked* requirement `|b| ≥ |a|`
+
+Computes `hi = fl(a+b)` and `lo = err(a+b)`.
+"""
+@inline function two_lohi_sum(a::T, b::T) where {T}
+    hi = b + a
+    lo = a - (hi - b)
+    return hi, lo
+end
+
+"""
+    two_hilo_diff(a, b)
+    
+*unchecked* requirement `|a| ≥ |b|`
+
+Computes `hi = fl(a-b)` and `lo = err(a-b)`.
+"""
+@inline function two_hilo_diff(a::T, b::T) where {T}
+    hi = a - b
+    lo = (a - hi) - b
+    hi, lo
+end
+
+"""
+    two_lohi_diff(a, b)
+    
+*unchecked* requirement `|b| ≥ |a|`
+
+Computes `hi = fl(a-b)` and `lo = err(a-b)`.
+"""
+@inline function two_lohi_diff(a::T, b::T) where {T}
+    hi = b - a
+    lo = (b - hi) - a
+    hi, lo
+end
+
+"""
+    three_hilo_diff(a, b, c)
+    
+*unchecked* requirement `|a| ≥ |b| ≥ |c|`
+
+Computes `x = fl(a-b-c)` and `y = err(a-b-c), z = err(y)`.
+"""
+function three_hilo_diff(a::T,b::T,c::T) where {T}
+    md, lo = two_hilo_sum(-b, -c)
+    hi, md = two_sum(a, md)
+    md, lo = two_hilo_sum(md, lo)
+    hi, md = two_hilo_sum(hi, md)
+    return hi,md,lo 
+end
+
+"""
+    three_lohi_diff(a, b, c)
+    
+*unchecked* requirement `|c| ≥ |b| ≥ |a|`
+
+Computes `x = fl(a-b-c)` and `y = err(a-b-c), z = err(y)`.
+"""
+function three_lohi_diff(c::T,b::T,a::T) where {T}
+    three_lohi_sum(c, -b, -a)
+end
+
+"""
+    four_hilo_diff(a, b, c, d)
+    
+*unchecked* requirement `|a| ≥ |b| ≥ |c| ≥ |d|`
+
+Computes `hi = fl(a-b-c-d)` and `hm = err(a-b-c-d), ml = err(hm), lo = err(ml)`.
+"""
+function four_hilo_diff(a::T,b::T,c::T,d::T) where {T}
+    t0, t1 = two_hilo_diff(a,  b)
+    t0, t2 = two_hilo_diff(t0,  c)
+    hi, t3 = two_hilo_diff(t0,  d)
+    t0, t1 = two_hilo_sum(t1, t2)
+    hm, t2 = two_hilo_sum(t0, t3) # here, t0 >= t3
+    ml, lo = two_hilo_sum(t1, t2)
+    return hi, hm, ml, lo
+end
+
+"""
+    four_hilo_diff(a, b, c, d)
+    
+*unchecked* requirement `|d| ≥ |c| ≥ |b| ≥ |a|`
+
+Computes `hi = fl(a-b-c-d)` and `hm = err(a-b-c-d), ml = err(hm), lo = err(ml)`.
+"""
+function four_lohi_diff(d::T,c::T,b::T,a::T) where {T}
+    t0, t1 = two_hilo_diff(a,  b)
+    t0, t2 = two_hilo_diff(t0,  c)
+    hi, t3 = two_hilo_diff(t0,  d)
+    t0, t1 = two_hilo_sum(t1, t2)
+    hm, t2 = two_hilo_sum(t0, t3) # here, t0 >= t3
+    ml, lo = two_hilo_sum(t1, t2)
+    return hi, hm, ml, lo
+end
