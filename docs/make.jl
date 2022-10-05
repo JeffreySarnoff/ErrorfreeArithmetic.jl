dc6d247e2ad16ddfda51ca679a7b79c4fb64773a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-10-05 02:15:13 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,46 @@
-using Documenter
-
 makedocs(
     modules = [ErrorfreeArithmetic],
     format = :html,
     sitename = "ErrorfreeArithmetic",
     authors = "Jeffrey A. Sarnoff and other contributors",
-    pages = [
-        "Package" => "index.md",
-        "Errorfree Arithmetic" => "intro.md",
-        "Basic usage" => "usage.md"
+    source="src",
+    clean=false,
+    strict=!("strict=false" in ARGS),
+    doctest=("doctest=only" in ARGS) ? :only : true,
+    format=Documenter.HTML(
+        # Use clean URLs, unless built as a "local" build
+        prettyurls=!("local" in ARGS),
+        highlights=["yaml"],
+        ansicolor=true,
+    ),
+    pages=[
+        "Home" => "index.md",
+        "Overview" => "overview.md",
+        "Introduction" => Any[
+            "Floating-Point Arithmetic"=>"intro/arithmetic.md",
+            "Accuracy and Precision"=>"intro/accuracy_precision.md",
+            "Extending Precision Accurately"=>"intro/extending_precision.md",
+            "Function Naming"=>"intro/naming.md",
+        ],
+        "API" => Any[
+            "add" => "api/addition.md",
+            "subtract" => "api/subtraction.md",
+            "multiply" => "api/multiply.md",
+            "divide" => "api/divide.md",
+            "fma" => "api/fma.md",
+            "others" => "api/others",
+        ],
     ]
 )
 
-deploydocs(
-    repo   = "github.com/JeffreySarnoff/ErrorfreeArithmetic.jl.git",
-    target = "build"
+#=
+Deploy docs to Github pages.
+=#
+Documenter.deploydocs(
+    branch = "gh-pages",
+    target = "build",
+    deps = nothing,
+    make = nothing,
+    repo = "github.com/JeffreySarnoff/ErrorfreeArithmetic.jl.git",
 )
 
