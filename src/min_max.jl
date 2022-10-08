242700b91932bee6a8e3ace8c9af1ce9ff78a73b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-10-08 08:56:45 -0400

    Update min_max.jl

--- src/min_max.jl
+++ src/min_max.jl
@@ -1,9 +1,5 @@
-"""
-    one_minmag(a, b)
-
-obtains value of least magnitude
-"""
-one_minmag(a::T, b::T) where {T} = abs(a) < abs(b) ? a : b 
+# <n>_maxmag
+# <n>_minmag below
 
 """
     one_maxmag(a, b)
@@ -11,15 +7,6 @@ one_minmag(a::T, b::T) where {T} = abs(a) < abs(b) ? a : b
 obtains value of greatest magnitude
 """
 one_maxmag(a::T, b::T) where {T} = abs(a) < abs(b) ? b : a 
-
-"""
-    one_minmag(a, b, c)
-
-obtains value with least magnitude
-"""
-function one_minmag(a::T, b::T, c::T) where {T}
-     one_minmag(one_minmag(a, b), c)
-end
 
 """
     one_maxmag(a, b, c)
@@ -31,15 +18,6 @@ function one_maxmag(a::T, b::T, c::T) where {T}
 end
 
 """
-    one_minmag(a, b, c, d)
-
-obtains value with least magnitude
-"""
-function one_minmag(a::T, b::T, c::T, d::T) where {T}
-     one_minmag(one_minmag(a, b), one_minmag(c, d))
-end
-
-"""
     one_maxmag(a, b, c, d)
 
 obtains value with largest magnitude
@@ -47,32 +25,14 @@ obtains value with largest magnitude
 function one_maxmag(a::T, b::T, c::T, d::T) where {T}
      one_maxmag(one_maxmag(a, b), one_maxmag(c, d))
 end
+
     
-"""
-    two_minmag(a, b)
-
-orders (a, b) by ascending magnitude
-"""
-two_minmag(a::T, b::T) where {T} = abs(a) < abs(b) ? (a, b) : (b, a)
-
 """
     two_maxmag(a, b)
 
 orders (a, b) by descending magnitude
 """
 two_maxmag(a::T, b::T) where {T} = abs(b) < abs(a) ? (a, b) : (b, a)
-
-"""
-    two_minmag(a, b, c)
-
-obtains two values in order of ascending magnitude
-"""
-function two_minmag(x::T, y::T, z::T) where {T}
-     y, z = two_minmag(y, z)
-     x = one_minmag(x, z)
-     x, y = two_minmag(x, y)
-     return x, y
-end
 
 """
     two_maxmag(a, b, c)
@@ -83,6 +43,94 @@ function two_maxmag(x::T, y::T, z::T) where {T}
      y, z = two_maxmag(y, z)
      x = one_maxmag(x, z)
      x, y = two_maxmag(x, y)
+     return x, y
+end
+
+"""
+    two_maxmag(a, b, c, d)
+
+obtains two values with largest magnitudes in order of descending magnitude
+"""
+function two_maxmag(a::T, b::T, c::T, d::T) where {T}
+    a, b = two_maxmag(a, b)
+    c, d = two_maxmag(c, d)
+    a, c = two_maxmag(a, c)
+    b = one_maxmag(b, d)
+    b = one_maxmag(b, c)
+    a, b
+end
+
+"""
+    three_maxmag(a, b, c)
+
+orders (a, b, c) by descending magnitude
+"""
+function three_maxmag(x::T, y::T, z::T) where {T}
+     y, z = two_maxmag(y, z)
+     x, z = two_maxmag(x, z)
+     x, y = two_maxmag(x, y)
+     return x, y, z
+end
+
+"""
+    four_maxmag(a, b, c, d)
+
+orders 3 of (a, b, c, d) by descending magnitude
+"""
+function three_maxmag(a::T, b::T, c::T, d::T) where {T}
+    a, b = two_maxmag(a, b)
+    c, d = two_maxmag(c, d)
+    a, c = two_maxmag(a, c)
+    b, d = two_maxmag(b, d)
+    b, c = two_maxmag(b, c)
+     
+    return a, b, c, d
+end
+
+# <n>_minmag
+# <n>_maxmag above
+    
+"""
+    one_minmag(a, b)
+
+obtains value of least magnitude
+"""
+one_minmag(a::T, b::T) where {T} = abs(a) < abs(b) ? a : b 
+
+"""
+    one_minmag(a, b, c)
+
+obtains value with least magnitude
+"""
+function one_minmag(a::T, b::T, c::T) where {T}
+     one_minmag(one_minmag(a, b), c)
+end
+
+"""
+    one_minmag(a, b, c, d)
+
+obtains value with least magnitude
+"""
+function one_minmag(a::T, b::T, c::T, d::T) where {T}
+     one_minmag(one_minmag(a, b), one_minmag(c, d))
+end
+    
+"""
+    two_minmag(a, b)
+
+orders (a, b) by ascending magnitude
+"""
+two_minmag(a::T, b::T) where {T} = abs(a) < abs(b) ? (a, b) : (b, a)
+
+"""
+    two_minmag(a, b, c)
+
+obtains two values in order of ascending magnitude
+"""
+function two_minmag(x::T, y::T, z::T) where {T}
+     y, z = two_minmag(y, z)
+     x = one_minmag(x, z)
+     x, y = two_minmag(x, y)
      return x, y
 end
 
@@ -101,20 +149,6 @@ function two_minmag(a::T, b::T, c::T, d::T) where {T}
 end
 
 """
-    two_maxmag(a, b, c, d)
-
-obtains two values with largest magnitudes in order of descending magnitude
-"""
-function two_maxmag(a::T, b::T, c::T, d::T) where {T}
-    a, b = two_maxmag(a, b)
-    c, d = two_maxmag(c, d)
-    a, c = two_maxmag(a, c)
-    b = one_maxmag(b, d)
-    b = one_maxmag(b, c)
-    a, b
-end
-
-"""
     three_minmag(a, b, c)
 
 orders (a, b, c) by ascending magnitude
@@ -123,18 +157,6 @@ function three_minmag(x::T, y::T, z::T) where {T}
      y, z = two_minmag(y, z)
      x, z = two_minmag(x, z)
      x, y = two_minmag(x, y)
-     return x, y, z
-end
-
-"""
-    three_maxmag(a, b, c)
-
-orders (a, b, c) by descending magnitude
-"""
-function three_maxmag(x::T, y::T, z::T) where {T}
-     y, z = two_maxmag(y, z)
-     x, z = two_maxmag(x, z)
-     x, y = two_maxmag(x, y)
      return x, y, z
 end
 
@@ -154,21 +176,6 @@ function three_minmag(a::T, b::T, c::T, d::T) where {T}
 end
 
 """
-    four_maxmag(a, b, c, d)
-
-orders 3 of (a, b, c, d) by descending magnitude
-"""
-function three_maxmag(a::T, b::T, c::T, d::T) where {T}
-    a, b = two_maxmag(a, b)
-    c, d = two_maxmag(c, d)
-    a, c = two_maxmag(a, c)
-    b, d = two_maxmag(b, d)
-    b, c = two_maxmag(b, c)
-     
-    return a, b, c
-end
-
-"""
     four_minmag(a, b, c, d)
 
 orders (a, b, c, d) by ascending magnitude
@@ -181,29 +188,4 @@ function four_minmag(a::T, b::T, c::T, d::T) where {T}
     b, c = two_minmag(b, c)
      
     return a, b, c, d
-end
-
-"""
-    four_maxmag(a, b, c, d)
-
-orders (a, b, c, d) by descending magnitude
-"""
-function four_maxmag(a::T, b::T, c::T, d::T) where {T}
-    a, b = two_maxmag(a, b)
-    c, d = two_maxmag(c, d)
-    a, c = two_maxmag(a, c)
-    b, d = two_maxmag(b, d)
-    b, c = two_maxmag(b, c)
-     
-    return a, b, c, d
-end
-
-# fastest sorting by decreasing magnitude
-amaxmin(x::T, y::T) where {T} = abs(x) < abs(y) ? (y,x) : (x,y)
-
-@inline function amaxmin(x::T, y::T, z::T) where {T}
-    y, z = amaxmin(y, z)
-    x, z = amaxmin(x, z)
-    x, y = amaxmin(x, y)
-    return x, y, z
 end
