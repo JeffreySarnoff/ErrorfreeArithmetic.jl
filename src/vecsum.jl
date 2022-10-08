c34977908cc1344b70acb34553a1ff7744659603
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-10-08 12:45:49 -0400

    Update vecsum.jl

--- src/vecsum.jl
+++ src/vecsum.jl
@@ -29,8 +29,9 @@ function vvec_sum(x0::T, x1::T, x2::T, x3::T) where {T}
     s1, e2 = two_sum(x1, s2)
     s0, e1 = two_sum(x0, s1)
     s2, e3 = two_sum(e2, e3)
-    s1, e2 = two_hilo_sum(e1, s2)
+    s1, e2 = two_sum(e1, s2)
     s0, e1 = two_hilo_sum(s0, s1)
+    e2, e3 = two_hilo_sum(e2, e3)
     return s0,e1,e2,e3
 end
 
