ab93240c59e491cca97b6f38e3b687b29e425147
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-10-07 23:53:25 -0400

    Update sum.jl

--- src/sum.jl
+++ src/sum.jl
@@ -176,12 +176,8 @@ Computes `hi = fl(a+b+c+d)` and `hm = err(a+b+c+d), ml = err(mh), lo = err(ml)`.
 function two_sum(a::T,b::T,c::T,d::T) where {T}
     hi, mh, ml, lo = four_maxmag(a, b, c, d)
     ml, lo  = two_hilo_sum(ml, lo)
-    mh, ml  = two_hilo_sum(mh, ml)
-    ml, lo  = two_hilo_sum(ml, lo)
-    mh, ml  = two_hilo_sum(mh, ml)
-    hi, mh  = two_hilo_sum(hi, mh)
-    mh, ml  = two_hilo_sum(mh, ml)
-    hi, mh  = two_hilo_sum(hi, mh)
+    mh, ml  = two_sum(mh, ml)
+    hi, mh  = two_sum(hi, mh)
     hi, mh
 end
 
