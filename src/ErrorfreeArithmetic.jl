35a74e3b2591123aa551a3ee0a9326954cf4d1bb
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-10-08 22:17:33 -0400

    Update ErrorfreeArithmetic.jl

--- src/ErrorfreeArithmetic.jl
+++ src/ErrorfreeArithmetic.jl
@@ -8,8 +8,8 @@ export # error-free transformations
        # error-free transformations with magnitude sorted arguments
        two_hilo_sum, three_hilo_sum, four_hilo_sum,
        two_lohi_sum, three_lohi_sum, four_lohi_sum,
-       two_hilo_diff, three_hilo_diff
-       two_lohi_diff, three_lohi_diff
+       two_hilo_diff, three_hilo_diff,
+       two_lohi_diff, three_lohi_diff,
        # least-error transformations, as close to error-free as possible
        two_inv, two_sqrt,
        two_div
