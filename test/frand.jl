c37624e6b3f0a091dded65e2d349e5fcf1d49d14
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-10-09 00:49:55 -0400

    Update frand.jl

--- test/frand.jl
+++ test/frand.jl
@@ -14,7 +14,19 @@ randexp(; range=0:63) = rand(rng, range)
 randexp(n=1; range=0:63) = rand(rng, range, n)
 
 randexp() = randexp(Float64)
-randexp(::Type{T}; shift=Base.exponent_bits(T)>>4) where {T<:AbstractFloat} = rand(rng, (1+exponent(floatmin(T))):(1+exponent(floatmax(T)))) >> shift
+randexp(::Type{T}) where {T<:AbstractFloat} = rand(rng, (1+exponent(floatmin(T))):(1+exponent(floatmax(T))))
+randexp(sr::StepRange{I,I}) where {I<:Integer} = rand(rng, sr)
 
-frand() = frand(Float64)
+randexp(n::Int) = randexp(rng, Float64, n)
+randexp(::Type{T}, n::Int) where {T<:AbstractFloat} = rand(rng, (1+exponent(floatmin(T))):(1+exponent(floatmax(T))), n)
+randexp(sr::StepRange{I,I}, n::Int) where {I<:Integer} = rand(rng, sr, n)
+
+frand() = rand(rng, Float64)
 frand(::Type{T}) where {T<:AbstractFloat} = ldexp(rand(T), randexp())
+frand(sr::StepRange{I,I}) where {T<:AbstractFloat, I<:Integer} = ldexp(rand(), randexp(sr))
+frand(::Type{T}, sr::StepRange{I,I}) where {T<:AbstractFloat, I<:Integer} = ldexp(rand(T), randexp(sr))
+
+frand(n::Int) = rand(rng, n)
+frand(::Type{T}, n::Int) where {T<:AbstractFloat} = map(ldexp, rand(rng, T, n), randexp(T,n))
+frand(sr::StepRange{I,I}, n::Int) where {T<:AbstractFloat, I<:Integer} = map(ldexp, rand(rng, n), randexp(sr, n))
+frand(::Type{T}, sr::StepRange{I,I}, n::Int) where {T<:AbstractFloat, I<:Integer} = ldexp(rand(rng, T), randexp(sr))
