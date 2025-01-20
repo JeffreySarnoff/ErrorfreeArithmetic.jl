f206364d09239af1bc04cde5ba931414d3ba4a7a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2025-01-20 06:36:40 -0500

    Update simd.jl

--- src/simd.jl
+++ src/simd.jl
@@ -4,6 +4,26 @@ const Float32x4 = NTuple{4, Base.VecElement{Float32}}
 const Float64x4 = NTuple{4, Base.VecElement{Float64}}
 const Float32x8 = NTuple{8, Base.VecElement{Float32}}
 
+for (T,N,C) in ((:Float32x2, :2, :float), (:Float32x4, :4, :float), (:Float32x8, :8, :float),  
+                (:Float64x2, :2, :double), (:Float64x4, :4, :double))
+    thecall = :("%res = fneg <$(N) x $(C) > %0\n ret <$(N) x $(C)> %res")        
+    fn = """function Base.:-(x::$(T))
+            Base.llvmcall($(thecall), $(T), Tuple{$(T)}, x)
+            end"""
+    @eval Meta.parse($fn)
+end
+
+for (T,N,C) in ((:Float32x2, :2, :float), (:Float32x4, :4, :float), (:Float32x8, :8, :float),  
+                (:Float64x2, :2, :double), (:Float64x4, :4, :double))
+    thecall = :("%res = fadd <$(N) x $(C) > %0\n ret <$(N) x $(C)> %res")        
+    fn = """function Base.:+(x::$(T), y::$(T))
+            Base.llvmcall($(thecall), $(T), Tuple{$(T), $(T)}, x, y)
+            end"""
+    println(fn)
+    @eval Meta.parse($fn)
+end
+
+
 function Base.:-(x::Float32x2)
     Base.llvmcall("""
         %res = fneg <2 x float> %0
