4a33c27e022d25f46499547978564e7b9095d8e2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2025-01-20 06:27:50 -0500

    Update simd.jl

--- src/simd.jl
+++ src/simd.jl
@@ -1,1 +1,33 @@
-using SIMD
+const Float32x2 = NTuple{2, Base.VecElement{Float32}}
+const Float64x2 = NTuple{2, Base.VecElement{Float64}}
+const Float32x4 = NTuple{4, Base.VecElement{Float32}}
+const Float64x4 = NTuple{4, Base.VecElement{Float64}}
+const Float32x8 = NTuple{8, Base.VecElement{Float32}}
+
+function Base.:-(x::Float32x2)
+    Base.llvmcall("""
+        %res = fneg <2 x float> %0
+        ret <2 x float> %res
+        """, Float32x2, Tuple{Float32x2}, x)
+end
+
+function Base.:-(x::Float32x4)
+    Base.llvmcall("""
+        %res = fneg <4 x float> %0
+        ret <4 x float> %res
+        """, Float32x4, Tuple{Float32x4}, x)
+end
+
+function Base.:-(x::Float64x2)
+    Base.llvmcall("""
+        %res = fneg <2 x double> %0
+        ret <2 x double> %res
+        """, Float64x4, Tuple{Float64x2}, x)
+end
+
+function Base.:-(x::Float64x4)
+    Base.llvmcall("""
+        %res = fneg <4 x double> %0
+        ret <4 x double> %res
+        """, Float64x4, Tuple{Float64x4}, x)
+end
